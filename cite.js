6fa2b0346b234735aef1fb478ffe8f14e51ef4ae
samhiner <25464398+samhiner@users.noreply.github.com>
2019-01-02 00:01:13 -0500

    Little bit of refactoring frontend citation.

--- cite.js
+++ cite.js
@@ -1,15 +1,15 @@
 //TODO refactor everything below
 	//TODO add documentation
 	//TODO add input validation (means make sure all areas are filled and it doesn't have unpredictable behavior when users do weird things)
-		//TODO find that bug from a while ago where there were two commas when you didn't add some optional info
+		//TODO figure out n.p.
 	//TODO make sure that people can't hack the site through the URL submit with XSS or something
 //TODO double spaced and page margins (maybe a document outline)
 	//TODO Analyze google docs to find out how they define margins in html and copy that for the citation output to see if it copies over to drive
 //TODO turn url into escape codes before giving it to server
 	//TODO should I convert it back to plain text for URL part of citation?
 //TODO Collect data on what percent of AutoCite's are successful
-//TODO figure out n.p.
 
+//add zeroes in front of an input until it fills the required spaces
 function leadingZero(input, spaces) {
 	if (input.length < spaces) {
 		return leadingZero('0' + input)
@@ -18,33 +18,37 @@ function leadingZero(input, spaces) {
 	}
 }
 
-function onStart() {
+//fill in the access date with the current date
+function setAccessDate() {
 	currDate = new Date();
 	currMonth = leadingZero(String(currDate.getMonth() + 1), 2);
 	currDay = leadingZero(String(currDate.getDate()), 2);
 
-	document.getElementById('accessDate').value = String(currDate.getFullYear()) + '-' + currMonth + '-' + currDay;
+	$('#accessDate').val(String(currDate.getFullYear()) + '-' + currMonth + '-' + currDay);
 }
 
-function authChange() {
-	let authNum = Number(document.getElementById('authNum').value);
+//show a different number of inputs for author based on the number specified by user
+function authorChange() {
+	let authorNum = Number($('#authorNum')[0].value);
 
-	let auth1 = document.getElementById('auth1').style.display;
-	let auth2 = document.getElementById('auth2').style.display;
+	let author1 = $('#author1').style.display;
+	let author2 = $('#author2').style.display;
 
-	if (authNum == 2) {
-		document.getElementById('auth1').style.display = 'block';
-		document.getElementById('auth2').style.display = 'block';
-	} else if ((authNum == 1) || (authNum > 2)) {
-		document.getElementById('auth1').style.display = 'block';
-		document.getElementById('auth2').style.display = 'none';
+	if (authorNum == 2) {
+		$('#author1').style.display = 'block';
+		$('#author2').style.display = 'block';
+	} else if ((authorNum == 1) || (authorNum > 2)) {
+		$('#author1').style.display = 'block';
+		$('#author2').style.display = 'none';
 	} else {
-		document.getElementById('auth1').style.display = 'none';
-		document.getElementById('auth2').style.display = 'none';
+		$('#author1').style.display = 'none';
+		$('#author2').style.display = 'none';
 	}
 
 }
 
+//turn name into MLA version of name based on position
+	//ord (1 <= int <= 2): where does it appear in citation. e.g. it would be 1 for John and 2 for Jack in "John and Jack"
 function createName(name, ord) {
 	if (name[1] == '') {
 		return name[0];
@@ -57,51 +61,45 @@ function createName(name, ord) {
 	}
 }
 
-function createInText(name) {
-	if (name[1] == '') {
-		return name[0]
-	} else {
-		return name[1]
-	}
-}
-
+//TODO break up this function
+//megafunction that cites based on given data
 function cite() {
-	let authNum = document.getElementById('authNum').value;
+	let authorNum = $('#authorNum')[0].value;
 
-	var name1 = createName([document.getElementById('firstName1').value, document.getElementById('lastName1').value], 1)
-	var name2 = createName([document.getElementById('firstName2').value, document.getElementById('lastName2').value], 2)
+	var name1 = createName([$('#firstName1')[0].value, $('#lastName1')[0].value], 1)
+	var name2 = createName([$('#firstName2')[0].value, $('#lastName2')[0].value], 2)
 
-	if (authNum > 2) {
-		var auth = name1 + ' et al. ';
-	} else if (authNum == 2) {
-		var auth = name1 + ', and ' + name2 + '. ';
-	} else if (authNum == 1) {
-		var auth = name1 + '. ';
+	if (authorNum > 2) {
+		var author = name1 + ' et al. ';
+	} else if (authorNum == 2) {
+		var author = name1 + ', and ' + name2 + '. ';
+	} else if (authorNum == 1) {
+		var author = name1 + '. ';
 	} else {
-		var auth = '';
+		var author = '';
 	}
 
-	var article = '"' + document.getElementById('article').value + '". ';
+	var article = '"' + $('#article')[0].value + '". ';
 
-	var website = '<span class="website">' + document.getElementById('website').value + '</span>, ';
+	var website = '<span class="website">' + $('#website')[0].value + '</span>, ';
 
-	var publisher = document.getElementById('publisher').value + ', ';
+	var publisher = $('#publisher')[0].value + ', ';
 
-	if (document.getElementById('publisher').value == document.getElementById('website').value) {
+	if ($('#publisher')[0].value == $('#website')[0].value) {
 		publisher = '';
 	}
 
-	if (document.getElementById('pubYear').value == -1) {
+	if ($('#pubYear')[0].value == -1) {
 		var pubDate = '';
 	} else {
-		var pubDate = document.getElementById('pubYear').value + ', ';
+		var pubDate = $('#pubYear')[0].value + ', ';
 	}
 
-	var url = document.getElementById('url').value + '. ';
+	var url = $('#url')[0].value + '. ';
 
 	var months = ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'June', 'July', 'Aug', 'Sept', 'Oct', 'Nov', 'Dec'];
 
-	var rawAccessDate = document.getElementById('accessDate').value.split('-')
+	var rawAccessDate = $('#accessDate')[0].value.split('-')
 
 	while (String(rawAccessDate[2])[0] == '0') {
 		rawAccessDate[2] = rawAccessDate[2].substr(1);
@@ -109,22 +107,22 @@ function cite() {
 
 	var accessDate = 'Accessed ' + rawAccessDate[2] + ' ' + months[rawAccessDate[1] - 1] + ' ' + rawAccessDate[0];
 
-	document.getElementById('citation').innerHTML = auth + article + website + publisher + pubDate + url + accessDate + '.';
+	$('#citation')[0].innerHTML = author + article + website + publisher + pubDate + url + accessDate + '.';
 
-	inText1 = createInText([document.getElementById('firstName1').value, document.getElementById('lastName1').value]);
-	inText2 = createInText([document.getElementById('firstName2').value, document.getElementById('lastName2').value]);
+	inTextName1 = $('#lastName1')[0].value == '' ? $('#firstName1')[0].value : $('#lastName1')[0].value;
+	inTextName2 = $('#lastName1')[0].value == '' ? $('#firstName1')[0].value : $('#lastName1')[0].value;
 
-	if (authNum == 1) {
-		var inTextCite = inText1;
-	} else if (authNum == 2) {
-		var inTextCite = inText1 + ', and ' + inText2;
-	} else if (authNum > 2) {
-		var inTextCite = inText1 + ' et al.';
+	if (authorNum == 1) {
+		var inTextCite = inTextName1;
+	} else if (authorNum == 2) {
+		var inTextCite = inTextName1 + ', and ' + inTextName2;
+	} else if (authorNum > 2) {
+		var inTextCite = inTextName1 + ' et al.';
 	} else {
-		var inTextCite = '"' + document.getElementById('article').value + '"';
+		var inTextCite = '"' + $('#article')[0].value + '"';
 	}
 
-	document.getElementById('in-text').innerHTML = '(' + inTextCite + ')';
+	$('#in-text')[0].innerHTML = '(' + inTextCite + ')';
 }
 
-onStart();
+setAccessDate();
