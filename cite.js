c791ca29431758ff36bf3aed602b34cb8c7e6742
samhiner <25464398+samhiner@users.noreply.github.com>
2019-01-02 21:48:31 -0500

    Experiment for citeproc.js
    I hope to use this to offer more citation formats.

--- cite.js
+++ cite.js
@@ -1,6 +1,7 @@
 //TODO refactor everything below
 	//TODO add documentation
 	//TODO add input validation (means make sure all areas are filled and it doesn't have unpredictable behavior when users do weird things)
+		//TODO for example if author is visible and not filled in make it requried
 		//TODO figure out n.p.
 	//TODO make sure that people can't hack the site through the URL submit with XSS or something
 //TODO double spaced and page margins (maybe a document outline)
@@ -29,20 +30,20 @@ function setAccessDate() {
 
 //show a different number of inputs for author based on the number specified by user
 function authorChange() {
-	let authorNum = Number($('#authorNum')[0].value);
+	let numAuthors = Number($('#numAuthors')[0].value);
 
-	let author1 = $('#author1').style.display;
-	let author2 = $('#author2').style.display;
+	let author1 = $('#author1')[0].style.display;
+	let author2 = $('#author2')[0].style.display;
 
-	if (authorNum == 2) {
-		$('#author1').style.display = 'block';
-		$('#author2').style.display = 'block';
-	} else if ((authorNum == 1) || (authorNum > 2)) {
-		$('#author1').style.display = 'block';
-		$('#author2').style.display = 'none';
+	if (numAuthors == 2) {
+		author1 = 'block';
+		author2 = 'block';
+	} else if ((numAuthors == 1) || (numAuthors > 2)) {
+		author1 = 'block';
+		author2 = 'none';
 	} else {
-		$('#author1').style.display = 'none';
-		$('#author2').style.display = 'none';
+		author1 = 'none';
+		author2 = 'none';
 	}
 
 }
@@ -50,38 +51,37 @@ function authorChange() {
 //turn name into MLA version of name based on position
 	//ord (1 <= int <= 2): where does it appear in citation. e.g. it would be 1 for John and 2 for Jack in "John and Jack"
 function createName(name, ord) {
-	if (name[1] == '') {
-		return name[0];
-	}
-
 	if (ord == 1) {
-		return name[1] + ', ' + name[0];
+		//if last name isn't filled, MLA name is only first name. Else, it is "last, first"
+		return name[1] == '' ? name[0] : name[1] + ', ' + name[0];
 	} else {
-		return name[0] + ' ' + name[1];
+		return name[1] == '' ? name[0] : name[0] + ' ' + name[1];
+	}
+}
+
+function createAuthorList(numAuthors) {
+	var name1 = createName([$('#firstName1')[0].value, $('#lastName1')[0].value], 1)
+
+	if (numAuthors > 2) {
+		var author = name1 + ' et al. ';
+	} else if (numAuthors == 2) {
+		var name2 = createName([$('#firstName2')[0].value, $('#lastName2')[0].value], 2)
+		var author = name1 + ', and ' + name2 + '. ';
+	} else if (numAuthors == 1) {
+		var author = name1 + '. ';
+	} else {
+		var author = '';
 	}
 }
 
 //TODO break up this function
 //megafunction that cites based on given data
 function cite() {
-	let authorNum = $('#authorNum')[0].value;
-
-	var name1 = createName([$('#firstName1')[0].value, $('#lastName1')[0].value], 1)
-	var name2 = createName([$('#firstName2')[0].value, $('#lastName2')[0].value], 2)
-
-	if (authorNum > 2) {
-		var author = name1 + ' et al. ';
-	} else if (authorNum == 2) {
-		var author = name1 + ', and ' + name2 + '. ';
-	} else if (authorNum == 1) {
-		var author = name1 + '. ';
-	} else {
-		var author = '';
-	}
+	let numAuthors = $('#numAuthors')[0].value;
 
 	var article = '"' + $('#article')[0].value + '". ';
 
-	var website = '<span class="website">' + $('#website')[0].value + '</span>, ';
+	var website = '<em>' + $('#website')[0].value + '</em>, ';
 
 	var publisher = $('#publisher')[0].value + ', ';
 
@@ -89,11 +89,7 @@ function cite() {
 		publisher = '';
 	}
 
-	if ($('#pubYear')[0].value == -1) {
-		var pubDate = '';
-	} else {
-		var pubDate = $('#pubYear')[0].value + ', ';
-	}
+	var pubYear = $('#pubYear')[0].value == '' ? '' : $('#pubYear')[0].value + ', ';
 
 	var url = $('#url')[0].value + '. ';
 
@@ -107,16 +103,16 @@ function cite() {
 
 	var accessDate = 'Accessed ' + rawAccessDate[2] + ' ' + months[rawAccessDate[1] - 1] + ' ' + rawAccessDate[0];
 
-	$('#citation')[0].innerHTML = author + article + website + publisher + pubDate + url + accessDate + '.';
+	$('#citation')[0].innerHTML = author + article + website + publisher + pubYear + url + accessDate + '.';
 
-	inTextName1 = $('#lastName1')[0].value == '' ? $('#firstName1')[0].value : $('#lastName1')[0].value;
-	inTextName2 = $('#lastName1')[0].value == '' ? $('#firstName1')[0].value : $('#lastName1')[0].value;
+	const inTextName1 = $('#lastName1')[0].value || $('#firstName1')[0].value;
 
-	if (authorNum == 1) {
+	if (numAuthors == 1) {
 		var inTextCite = inTextName1;
-	} else if (authorNum == 2) {
+	} else if (numAuthors == 2) {
+		const inTextName2 = $('#lastName2')[0].value || $('#firstName2')[0].value;
 		var inTextCite = inTextName1 + ', and ' + inTextName2;
-	} else if (authorNum > 2) {
+	} else if (numAuthors > 2) {
 		var inTextCite = inTextName1 + ' et al.';
 	} else {
 		var inTextCite = '"' + $('#article')[0].value + '"';
